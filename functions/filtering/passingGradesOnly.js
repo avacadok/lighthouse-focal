ae01c26108d1fc9fa1e1c6203807f6985fd51970
avacadok <avakuang8@gmail.com>
2022-06-02 17:56:20 -0700

    examples of different function

--- functions/filtering/passingGradesOnly.js
+++ functions/filtering/passingGradesOnly.js
@@ -2,7 +2,17 @@ const grades = [73, 69, 3, 100, 50, 70, 69, 88, 95, 77, 35];
 
 //return numbers that are 70 or above
 //num is a call back function
+
+//anonymous arrow function
 const pass = grades.filter(num => {
+  return num >= 70;
+});
+//anonymous function
+const passNum = grades.filter(function(num){
+  return num >= 70;
+});
+//call back function
+const passGrade = grades.filter(function callback(num){
   return num >= 70;
 });
 
