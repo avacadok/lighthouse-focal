bf62b97f08aa69aef2222ddd5a1a850c4ea82d80
AVacAdok <avakuang8@gmail.com>
2022-06-04 04:20:30 +0000

    refactor code make function operates on library are properties of library object & use (this) method

---
+++ newLibrary.js
@@ -0,0 +1,51 @@
+const library = {
+  tracks: { t01: { id: "t01",
+    name: "Code Monkey",
+    artist: "Jonathan Coulton",
+    album: "Thing a Week Three" },
+  t02: { id: "t02",
+    name: "Model View Controller",
+    artist: "James Dempsey",
+    album: "WWDC 2003"},
+  t03: { id: "t03",
+    name: "Four Thirty-Three",
+    artist: "John Cage",
+    album: "Woodstock 1952"}
+  },
+  playlists: { p01: { id: "p01",
+    name: "Coding Music",
+    tracks: ["t01", "t02"]
+  },
+  p02: { id: "p02",
+    name: "Other Playlist",
+    tracks: ["t03"]
+  }
+  },
+
+  // it's now a method!
+  printPlaylists: function() {
+    for (let key in this.playlists) {
+      //key= p01 & p02
+      const playlist = this.playlists[key]; //access the p01 object
+      console.log(key + ": " + playlist.name + " - " + playlist.tracks.length + " tracks");
+
+    }
+    
+  },
+
+  // it's now a method!
+  printTracks: function() {
+
+    for (let key in this.tracks) {
+      //access to properties that are inside p01
+      let track = this.tracks[key];
+      console.log(`${key}: ${track.name} by ${track.artist} (${track.album})`);
+    }
+  }
+
+  // ...
+
+};
+
+library.printPlaylists();
+library.printTracks();
